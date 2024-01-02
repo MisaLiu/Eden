0e080301aac3b8fd914cdfd51c4f37e6a27c965d
Misa Liu <misaliu@misaliu.top>
2024-01-02 23:19:46 +0800

    add unzip script

---
+++ scripts/unzip-apk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ..;
+
+if [ ! -f './Eden.apk' ]; then
+  echo 'No apk found! Aborting...';
+  exit 1;
+fi
+
+if [ -d './apk' ]; then
+  echo 'Found old /apk dir, deleting...';
+  rm -rf ./apk
+fi
+
+echo 'Extracting apk...';
+mkdir './apk';
+unzip -n ./Eden.apk -d ./apk;
+
+echo 'Extracting done';
