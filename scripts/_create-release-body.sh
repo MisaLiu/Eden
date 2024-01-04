b99baa35a9611d9bda0917f5a617625ab9611594
HIMlaoS_Misa <misaliu@misaliu.top>
2024-01-05 02:16:28 +0800

    fix typo

--- scripts/_create-release-body.sh
+++ scripts/_create-release-body.sh
@@ -5,7 +5,7 @@ FILE_VERSION=$(echo $FILE_NAME | grep -o "\([0-9\.]\+\)" | awk 'NR==1')
 FILE_MD5=$(cat ./Eden.apk | md5sum | cut -d " " -f1)
 RESULT_MD5=$(cat ./output/result.zip | md5sum | cut -d " " -f1)
 
-echo "# Rnu result for version \`$FILE_VERSION\`
+echo "# Run result for version \`$FILE_VERSION\`
 * URL: \`$APK_URL\`
 * MD5: \`$FILE_MD5\`
 * Version: \`$FILE_VERSION\`
