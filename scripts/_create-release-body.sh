53173a65466ba275aee5b284f693c244c749fb2f
HIMlaoS_Misa <misaliu@misaliu.top>
2024-01-05 17:49:59 +0800

    update ci script

--- scripts/_create-release-body.sh
+++ scripts/_create-release-body.sh
@@ -3,7 +3,7 @@
 FILE_NAME=$(basename $APK_URL)
 FILE_VERSION=$(echo $FILE_NAME | grep -o "\([0-9\.]\+\)" | awk 'NR==1')
 FILE_MD5=$(cat ./Eden.apk | md5sum | cut -d " " -f1)
-RESULT_MD5=$(cat ./output/result.zip | md5sum | cut -d " " -f1)
+RESULT_MD5=$(cat ./result.zip | md5sum | cut -d " " -f1)
 
 echo "# Run result for version \`$FILE_VERSION\`
 * URL: \`$APK_URL\`
